9b9fe7c5480820199f5db2c64672ca8f4eb8266a
OfficialParsaNaderi <parsanaderi1387.programmer@gmail.com>
2025-02-01 17:58:16 +0330

    Uploading CheatSheet .!

---
+++ P1.py
@@ -0,0 +1,84 @@
+#Term1
+# Date Types = (str) - (int) - (float)
+# (bool)
+# True , False
+# (list)[] - (Tuple)()
+# (dict){}
+#variable:name=value
+#______________________________________
+#python:
+#don't first number
+#don't Space letter
+#don't write the negative
+#_______________________________________
+#if:give value_else: don't give value_elif:give value
+#math operators:"+,-,*,/,//,**,%"
+#print("the weather is cold") if weather<20 else print("please stay at home")
+#number1,number2=int(input(">>")),int(input(">>"))
+#print(f"result:{number1*number2}")
+#print(input("<>"))
+#________________________________________
+#libraries : turtle . tkinter . os ...
+#from os import system
+#system("cls")
+#lower(parsa)-upper(PARSA)-capeitalize(PARSA)-title(Parsa)-replace(😁)
+#________________________________________
+#function()
+#define()
+#variable in function:parameter=arqument
+#global(...)out of def
+# def squared(number) :
+#    return number**2
+#Value = squared(number = int(input(" What is Your Fav Number ?! \n >>")))
+#print(Value
+#__________________________________________
+#slicing : str , list
+#names = ["parsa","poyan","mother","father"]#list
+#others_names = ("parsa","poyan","mother","father")#tuple
+#__________________________________________
+#r : read
+#r+: read and write
+#w : make file for write
+#w+: make file for write and read
+#a : read(start from end)
+#a+: read and write(start from end)
+#open , write/read , close
+#append(a),remove(r)
+#read lines : make a list for your line
+#___________________________________________
+# Dictionary : (key = value) -> Item
+#___________________________________________
+#from random import randint,randrange,choice
+#from string import ascii_letters
+#___________________________________________
+#Term2
+# points = ( color = any color example = red , blue , ...) , ( write = any writes example = Parsa , Poyan , ...) ,
+# ( size = any size example = 600x200 ,... : point : we must write "x" between two size ) , ( icon = you can write any icon but must your icon have example = icon.ico )
+# ( width = have 15 word are tkinter ( give size of tkinter writing )) , ( mainloop = similar done() of turtle)
+#___________________________________________
+# from tkinter import Tk , Label , Entry , PhotoImage
+# root1 = Tk()
+# root1["bg"] = "(color)"
+# root1.title( string = " write ")
+# root1.geometry("(size)")
+# root1.iconbitmap("image/(icon).ico")
+# root1.state(n)
+# root2 = Label ( master = root1 , text = "(write)" , bg = "(color)" ,
+#   fg = "(color)" , font = ("JetBrainsMono Nerd Font Propo" , 10))
+# root3 = Entry ( master = root1 , font = (" " , 10) , width = 15 )
+# root2.pack()
+# root3.pack()
+# root1.mainloop()
+#___________________________________________
+#Below is a list of all data types in NumPy and the characters used to represent them.
+#i - integer
+#b - boolean
+#u - unsigned integer
+#f - float
+#c - complex float
+#m - timedelta
+#M - datetime
+#O - object
+#S - string
+#U - unicode string
+#V - fixed chunk of memory for other type ( void )
